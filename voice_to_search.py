cde6c31584a108fc244a775d36b03da98712f94e
SR-Dhana-shree <116423372+SR-Dhana-shree@users.noreply.github.com>
2022-10-22 15:16:19 +0530

    Update voice_to_search.py

--- voice_to_search.py
+++ voice_to_search.py
@@ -1,21 +1,26 @@
+#imorting essential libraries
 import speech_recognition as sr
 import webbrowser
 
+#for accessing microphone
 sr.Microphone(device_index=1)
 
-r=sr.Recognizer()
+#calling recognizer function
+q=sr.Recognizer()
+q.energy_threshold=5000
 
-
-r.energy_threshold=5000
-
+#main program
 with sr.Microphone() as source:
     print("Speak!")
-    audio=r.listen(source)
+    audio=q.listen(source)
     try:
-        text=r.recognize_google(audio)
+        text=q.recognize_google(audio)
         print("You said : {}".format(text))
         url='https://www.google.co.in/search?q='
+
+        #concatenate the url and given audio
         search_url=url+text
         webbrowser.open(search_url)
     except:
         print("Can't recognize")
+
